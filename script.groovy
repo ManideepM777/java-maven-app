5431153e37c1dd7c8cc9bfc13401d40d3aea549b
ManideepM777 <145978628+ManideepM777@users.noreply.github.com>
2024-08-07 21:01:25 +0530

    Update script.groovy

--- script.groovy
+++ script.groovy
@@ -5,7 +5,7 @@ def buildJar() {
 
 def buildImage() {
     echo "building the docker image..."
-    withCredentials([usernamePassword(credentialsId: 'dockerhub', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
+    withCredentials([usernamePassword(credentialsId: 'docker', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
         sh 'docker build -t manideepm777/maven-demo:1.0 .'
         sh "echo $PASS | docker login -u $USER --password-stdin"
         sh 'docker push manideepm777/maven-demo:1.0'
